cfd5ca1b7b72994496435dbaf7fd99482ab70ca0
thisismypassport <thisismypassport@noemail.com>
2023-08-30 05:24:05 +0300

    ignore compressed-looking strings when collecting chars

--- pico_rename.py
+++ pico_rename.py
@@ -148,6 +148,9 @@ def rename_tokens(ctxt, root, rename_opts):
                 for ch in sublang.get_unminified_chars():
                     char_uses[ch] += 1
             else:
+                if token.type == TokenType.string and len(set(token.value)) >= 0x80: # ignore compressed strings (heuristic)
+                    return
+
                 for ch in token.value:
                     char_uses[ch] += 1
 
