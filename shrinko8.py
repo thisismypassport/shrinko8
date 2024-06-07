945772915fc5fbe3ab9b5da0ae2abb8d41f00ecd
thisismypassport <thisismypassport@noemail.com>
2024-06-07 05:41:07 +0300

    part 2 of commit - moved all the shrinko8.py logic to shrinko.py, leaving shrinko8 as a wrapper like shrinkotron

---
+++ shrinko8.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from shrinko import create_main
+from pico_defs import Language
+import sys
+
+main = create_main(Language.pico8)
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
